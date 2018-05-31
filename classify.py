6e3cad0ea9fb26e4882cdb87f456dc2cfc74621d
Axel Runnholm <axel.runnholm@astro.su.se>
2018-05-31 13:14:58 +0200

    Add file that will contain classification script

---
+++ classify.py
@@ -0,0 +1,13 @@
+import SDSSmanagement as sdss
+import pandas as pd
+import numpy as np
+from astropy import coordinates as coords
+from astropy import units as u
+from astropy import table
+from astropy.io import ascii as save_asc
+
+
+def main():
+
+if __name__ == '__main__':
+    main()
